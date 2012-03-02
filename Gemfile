37ff61615d692af43872da3c2c0616de01fbf097
Jay Ricky B. Villarante <jbvillarante@Jays-MacBook-Air.local>
2012-03-02 09:42:13 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.1'
+gem 'mysql2'
+gem 'devise'
+gem 'omniauth'
+gem 'nokogiri'
+#gem 'rails_admin', :git => "https://github.com/sferik/rails_admin.git", :ref => '1ccd2fabce712ca1cb154fd17fbd71d6a8a305e0'
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'rspec2-rails-views-matchers'
+  gem 'rspec-prof'
+  gem 'rr'
+  gem 'fixture_builder'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeamGutom::Application.config.secret_token = '9bf0f52001ec1525186a89a7520c89321fca910c00c4c464fe93560b6922b2b9270331fd0b1f826c08f058870eb356675f92f9a33377ec924fd2b415b8737c4d'
