d2f8b888baeb536d80e2d3538f7dc7b6a053d5c7
Jay Ricky B. Villarante <jbvillarante@Jays-MacBook-Air.local>
2012-03-04 03:45:26 +0800

    add rails_admin, add users table

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.2'
-gem 'mysql2', '~> 0.2.7'
+gem 'rails', '3.1.4'
+gem 'mysql2', '~> 0.3.10'
 gem 'devise'
-gem 'omniauth'
 gem 'nokogiri'
-#gem 'rails_admin', :git => "https://github.com/sferik/rails_admin.git", :ref => '1ccd2fabce712ca1cb154fd17fbd71d6a8a305e0'
+gem 'fastercsv'
+gem 'rails_admin', :git => "https://github.com/sferik/rails_admin.git", :ref => '1ccd2fabce712ca1cb154fd17fbd71d6a8a305e0'
 
 group :test, :development do
   gem 'rspec-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,6 +11,35 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120303192014) do
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+
+  create_table "users", :force => true do |t|
+    t.string   "email",               :default => "", :null => false
+    t.string   "encrypted_password",  :default => "", :null => false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.boolean  "admin"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
