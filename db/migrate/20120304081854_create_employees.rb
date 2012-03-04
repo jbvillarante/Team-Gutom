b92e0ec6d70bdd960d9ded1caaa224500d9f3777
Liz <chit@chit-1015PEM.(none)>
2012-03-04 16:20:09 +0800

    rails generate model Employee

---
+++ db/migrate/20120304081854_create_employees.rb
@@ -0,0 +1,18 @@
+class CreateEmployees < ActiveRecord::Migration
+  def change
+    create_table :employees do |t|
+      t.string :lastname
+      t.string :firstname
+      t.string :role
+      t.integer :salary
+      t.integer :sss_no
+      t.integer :tin
+      t.integer :philhealth
+      t.string :address
+      t.string :telephone_no
+      t.string :email
+
+      t.timestamps
+    end
+  end
+end
