712ad9ed9730c61c9be6a03331cf153cf7f1e07f
Liz <chit@chit-1015PEM.(none)>
2012-03-16 09:44:39 +0800

    fixed sss attribute

--- app/models/populator.rb
+++ app/models/populator.rb
@@ -12,7 +12,7 @@ class Populator
     (1..n).each do |k|
       Employee.create!(:lastname => Forgery::Name.last_name, :firstname => Forgery::Name.first_name,
                        :role => Employee::Roles[rand(5)], :salary => Forgery(:monetary).money(:min => 9000,:max => 30000),
-                       :sss => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
+                       :sss_no => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
                        :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
                        :philhealth => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
                        :address => "#{Forgery(:address).street_address}, #{Forgery(:address).city} #{Forgery(:address).province}, #{Forgery(:address).country} #{Forgery(:address).zip}",
