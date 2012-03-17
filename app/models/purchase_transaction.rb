ce2ee981744f2771e8ffb48e80517fe24f42e6f2
Jay Ricky B. Villarante <jbvillarante@Jays-MacBook-Air.local>
2012-03-18 01:05:06 +0800

    fix dependencies and some more UI enhancements

--- app/models/purchase_transaction.rb
+++ app/models/purchase_transaction.rb
@@ -2,7 +2,25 @@ class PurchaseTransaction < ActiveRecord::Base
   belongs_to :employee
   has_many :purchase_materials
 
+  after_create :add_material_stocks
+  after_destroy :deduct_material_stocks
+
   def name
     "#{id} - #{created_at.strftime("%b %d, %Y")}" if created_at.present?
+  end
+
+  private
+
+  def add_material_stocks
+    purchase_materials.each do |pm|
+      pm.raw_material.update_attribute(:stock_qty, pm.raw_material.stock_qty + pm.quantity)
+    end
+  end
+
+  def deduct_material_stocks
+    purchase_materials.each do |pm|
+      pm.raw_material.update_attribute(:stock_qty, pm.raw_material.stock_qty - pm.quantity)
+      pm.destroy
+    end
   end
 end
--- app/models/sale_transaction.rb
+++ app/models/sale_transaction.rb
@@ -5,7 +5,26 @@ class SaleTransaction < ActiveRecord::Base
 
   has_many :sale_products
 
+  after_create :deduct_product_stocks
+  after_destroy :add_product_stocks
+
   def name
     "#{id} - #{created_at.strftime("%b %d, %Y")}" if created_at.present?
+  end
+
+  private
+  
+  def deduct_product_stocks
+    sale_products.each do |sp|
+      sp.product.update_attribute(:stock_qty, sp.product.stock_qty - sp.quantity)
+    end
+  end
+
+  def add_product_stocks
+    sale_products.each do |sp|
+      sp.product.update_attribute(:stock_qty, sp.product.stock_qty + sp.quantity)
+      sp.destroy
+      delivery.try(:destroy)
+    end
   end
 end
