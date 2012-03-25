6684492b122513c6958085efbc24200d7635a0a9
Liz <chit@chit-1015PEM.(none)>
2012-03-26 00:15:06 +0800

    changed range of data for pg

--- app/models/populator.rb
+++ app/models/populator.rb
@@ -12,9 +12,9 @@ class Populator
     (1..n).each do |k|
       Employee.create!(:lastname => Forgery::Name.last_name, :firstname => Forgery::Name.first_name,
                        :role => Employee::Roles[rand(5)], :salary => Forgery(:monetary).money(:min => 9000,:max => 30000),
-                       :sss_no => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
-                       :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
-                       :philhealth => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000),
+                       :sss_no => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
+                       :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
+                       :philhealth => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
                        :address => "#{Forgery(:address).street_address}, #{Forgery(:address).city} #{Forgery(:address).province}, #{Forgery(:address).country} #{Forgery(:address).zip}",
                        :telephone_no => Forgery(:address).phone, :email => Forgery(:internet).email_address
                        )
@@ -26,7 +26,7 @@ class Populator
       Supplier.create!(:name => Forgery::Name.full_name,
                        :address => "#{Forgery(:address).street_address}, #{Forgery(:address).city} #{Forgery(:address).province}, #{Forgery(:address).country} #{Forgery(:address).zip}",
                        :telephone_no => Forgery(:address).phone, :email => Forgery(:internet).email_address,
-                       :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000000))
+                       :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000))
     end
   end
 
