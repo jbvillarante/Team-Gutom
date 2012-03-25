a467fd3f967823c4f62da7b4a2cd1eb4ab83d87b
Liz <chit@chit-1015PEM.(none)>
2012-03-26 00:23:33 +0800

    changed range of data for pg

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@ end
 
 Employee.create!(:lastname => Forgery::Name.last_name, :firstname => Forgery::Name.first_name,
                  :role => 'admin', :salary => Forgery(:monetary).money(:min => 9000,:max => 30000),
-                 :sss_no => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
-                 :tin => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
-                 :philhealth => Forgery(:basic).number(:at_least => 10000000000, :at_most => 1000000000000),
+                 :sss_no => Forgery(:basic).number(:at_least => 1000000000, :at_most => 2147483640),
+                 :tin => Forgery(:basic).number(:at_least => 1000000000, :at_most => 2147483640),
+                 :philhealth => Forgery(:basic).number(:at_least => 1000000000, :at_most => 2147483640),
                  :address => "#{Forgery(:address).street_address}, #{Forgery(:address).city} #{Forgery(:address).province}, #{Forgery(:address).country} #{Forgery(:address).zip}",
                  :telephone_no => Forgery(:address).phone, :email => 'admin@admin.com'
                 )
